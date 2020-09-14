6cad063bcbd259f6bfc655d9ea9922f2ce5a1454
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-14 21:43:02 +0100

    Updated the "UPDATE" routes and functions.

--- app/server/database/database.py
+++ app/server/database/database.py
@@ -1,10 +1,8 @@
-
 import motor.motor_asyncio
-
 from bson import ObjectId
 from decouple import config
-from app.server.database.database_helper import student_helper
 
+from app.server.database.database_helper import student_helper
 
 MONGO_DETAILS = config('MONGO_DETAILS')
 
@@ -21,57 +19,32 @@ async def retrieve_students():
         students.append(student_helper(student))
     return students
 
-# Add a new student into the database
+
 async def add_student(student_data: dict) -> dict:
     student = await student_collection.insert_one(student_data)
     new_student = await student_collection.find_one({"_id": student.inserted_id})
     return student_helper(new_student)
 
-# Retrieve a student with matching ID
+
 async def retrieve_student(id: str) -> dict:
     student = await student_collection.find_one({"_id": ObjectId(id)})
     if student:
         return student_helper(student)
 
-# Delete a student from the database
+
 async def delete_student(id: str):
     student = await student_collection.find_one({"_id": ObjectId(id)})
     if student:
         await student_collection.delete_one({"_id": ObjectId(id)})
         return True
 
-# Update a student name
-async def update_student_name(id: str, fullname: str):
+
+async def update_student(id: str, data: dict):
+    if len(data) < 1:
+        return False
     student = await student_collection.find_one({"_id": ObjectId(id)})
     if student:
-        student_collection.update_one({"_id": ObjectId(id)}, {"$set": {"fullname": fullname}})
-        return True
-
-# Update a student email
-async def update_student_email(id: str, email: str):
-    student = await student_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        student_collection.update_one({"_id": ObjectId(id)}, {"$set": {"email": email}})
-        return True
-
-# Update a student course
-async def update_student_course(id: str, course: str):
-    student = await student_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        student_collection.update_one({"_id": ObjectId(id)}, {"$set": {"course": course}})
-        return True
-
-# Update a student year
-async def update_student_year(id: str, year: int):
-    student = await student_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        student_collection.update_one({"_id": ObjectId(id)}, {"$set": {"year": year}})
-        return True
-
-# Update a student GPA
-async def update_student_gpa(id: str, gpa: float):
-    student = await student_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        student_collection.update_one({"_id": ObjectId(id)}, {"$set": {"gpa": gpa}})
-        return True
-
+        updated_student = await student_collection.update_one({"_id": ObjectId(id)}, {"$set": data})
+        if updated_student:
+            return True
+        return False
