a4561d6f64c63434c32258fec23e871faf2506ec
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-16 01:52:05 +0100

    Schmea validations

--- app/server/models/student.py
+++ app/server/models/student.py
@@ -1,40 +1,44 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, PositiveFloat
+
 
 class StudentSchema(BaseModel):
     fullname: str = Field(...)
     email: EmailStr = Field(...)
     course_of_study: str = Field(...)
-    year: int = Field(...)
-    gpa: float = Field(...)
+    year: int = Field(..., gt=1, lt=9)
+    gpa: float = Field(..., le=4.0)
+
 
     class Config:
         schema_extra = {
             "example": {
-                "fullname": "Abdulazeez Abdulazeez Adeshina",
-                "email": "abdulazeez@x.edu.ng",
+                "fullname": "John Doe",
+                "email": "jdoe@x.edu.ng",
                 "course_of_study": "Water resources engineering",
                 "year": 2,
-                "gpa": "3.0"
+                "gpa": "3.0",
             }
         }
+
+
 
 class UpdateStudentModel(BaseModel):
     fullname: Optional[str]
     email: Optional[EmailStr]
     course_of_study: Optional[str]
     year: Optional[int]
-    gpa: Optional[float]
+    gpa: Optional[PositiveFloat]
 
     class Config:
         schema_extra = {
             "example": {
-                "fullname": "Abdulazeez Abdulazeez",
-                "email": "abdulazeez@y.edu.ng",
+                "fullname": "John Doe",
+                "email": "jdoe@x.edu.ng",
                 "course_of_study": "Water resources and environmental engineering",
                 "year": 4,
-                "gpa": "5.0"
+                "gpa": "4.0",
             }
         }
 
