6475e3c67cc6571532db900b7932fd0a6549d266
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-14 09:11:58 -0400

    Fix `ModuleNotFoundError: No module named app`

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
-from app.server.database import (
+from server.database import (
     add_student,
     delete_student,
     retrieve_student,
     retrieve_students,
     update_student,
 )
-from app.server.models.student import (
+from server.models.student import (
     ErrorResponseModel,
     ResponseModel,
     StudentSchema,
